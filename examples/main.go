d4f0b900b65a0d787bb90dff4cda12ad663816a3
ljg-cqu <ljg_cqu@126.com>
2022-03-05 10:48:34 +0800

    Apply customized Gin engine in example.

--- examples/main.go
+++ examples/main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"github.com/long2ice/swagin"
 	"github.com/long2ice/swagin/security"
 )
 
 func main() {
-	app := swagin.New(NewSwagger())
-	subApp := swagin.New(NewSwagger())
+	// Use customize Gin engine
+	r := gin.New()
+
+	// Registering func(c *gin.Context) is accepted,
+	// but the OpenAPI generator will ignore the operation and it won't appear in the specification.
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	app := swagin.NewFromEngine(r, NewSwagger())
+	subApp := swagin.NewFromEngine(r, NewSwagger())
+
+	/*
+		You can use default Gin engin:
+			app := swagin.New(NewSwagger())
+			subApp := swagin.New(NewSwagger())
+	*/
+
 	subApp.GET("/noModel", noModel)
 	app.Mount("/sub", subApp)
 	app.Use(cors.New(cors.Config{
@@ -29,7 +49,10 @@ func main() {
 	formGroup.PUT("", body)
 	formGroup.POST("/file", file)
 
-	if err := app.Run(); err != nil {
+	port := ":8084"
+
+	fmt.Printf("Now you can visit http://127.0.0.1%v/docs or http://127.0.0.1%v/redoc to see the api docs. Have fun!", port, port)
+	if err := app.Run(port); err != nil {
 		panic(err)
 	}
 }
